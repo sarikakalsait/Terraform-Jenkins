c6cb5202d3fef1448657fd87cdaa3802451f3661
Sarika Kalsait <71684198+sarikakalsait@users.noreply.github.com>
2025-04-09 23:44:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,8 +2,8 @@ provider "aws" {
     region = "us-east-1"  
 }
 
-resource "aws_instance" "foo" {
-  ami           = "ami-05fa00d4c63e32376" # us-west-2
+resource "aws_instance" "myec2" {
+  ami           = "ami-002f6e91abff6eb96"
   instance_type = "t2.micro"
   tags = {
       Name = "TF-Instance"
